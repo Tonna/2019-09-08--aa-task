fdcc2220c8288707b3bba55f7e4bb5570e0613ad
Anton Yakovchuk <yakovchuk.a.p@gmail.com>
2019-09-08 13:29:20 +0300

    added dependency and build steps. artifact added to repo.

--- draft-test.sh
+++ draft-test.sh
@@ -1,4 +1,6 @@
-test0=$(java -jar classes/artifacts/agile_engine_jar/agile-engine.jar src/test/data/sample-0-origin.html src/test/data/sample-0-origin.html)
+executable="target/agile-engine-1.0-jar-with-dependencies.jar"
+
+test0=$(java -jar "$executable" src/test/data/sample-0-origin.html src/test/data/sample-0-origin.html)
 
 if [ "$test0" = "/html/body" ]
   then printf "test0 success\n"
@@ -6,26 +8,26 @@ if [ "$test0" = "/html/body" ]
 fi
 
 
-test1=$(java -jar classes/artifacts/agile_engine_jar/agile-engine.jar src/test/data/sample-0-origin.html src/test/data/sample-1-evil-gemini.html)
+test1=$(java -jar "$executable" src/test/data/sample-0-origin.html src/test/data/sample-1-evil-gemini.html)
 
 if [ "$test1" = "/html/body" ]
   then printf "test1 success\n"
   else printf "test1 failed. returned \"%s\"\n" "$test1"
 fi
 
-test2=$(java -jar classes/artifacts/agile_engine_jar/agile-engine.jar src/test/data/sample-0-origin.html src/test/data/sample-2-container-and-clone.html)
+test2=$(java -jar "$executable" src/test/data/sample-0-origin.html src/test/data/sample-2-container-and-clone.html)
 if [ "$test2" = "/html/body" ]
   then printf "test2 success\n"
   else printf "test2 failed. returned \"%s\"\n" "$test2"
 fi
 
-test3=$(java -jar classes/artifacts/agile_engine_jar/agile-engine.jar src/test/data/sample-0-origin.html src/test/data/sample-3-the-escape.html)
+test3=$(java -jar "$executable" src/test/data/sample-0-origin.html src/test/data/sample-3-the-escape.html)
 if [ "$test3" = "/html/body" ]
   then printf "test3 success\n"
   else printf "test3 failed. returned \"%s\"\n" "$test3"
 fi
 
-test4=$(java -jar classes/artifacts/agile_engine_jar/agile-engine.jar src/test/data/sample-0-origin.html src/test/data/sample-4-the-mash.html)
+test4=$(java -jar "$executable" src/test/data/sample-0-origin.html src/test/data/sample-4-the-mash.html)
 if [ "$test4" = "/html/body" ]
   then printf "test4 success\n"
   else printf "test4 failed. returned \"%s\"\n" "$test4"
--- src/main/java/com/yakovcha/Main.java
+++ src/main/java/com/yakovcha/Main.java
@@ -5,16 +5,37 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class Main {
+
+    public static final String DEFAULT_ID = "make-everything-ok-button";
+
     public static void main(String[] args) throws IOException {
-        if(args.length < 2) {
+        if (args.length < 2) {
             throw new IllegalArgumentException("please provide two file pathes");
         }
         String firstFilePath = args[0];
         String secondFilePath = args[1];
 
+        //optional
+        String origId;
+        if (args.length == 3) {
+            origId = args[2];
+        } else {
+            origId = DEFAULT_ID;
+        }
+
         List<String> orig = Files.readAllLines(Paths.get(firstFilePath));
         List<String> toDiff = Files.readAllLines(Paths.get(secondFilePath));
+
+
+        Document doc = Jsoup.parse(String.join("", orig));
+        Elements origBtn = doc.select("#" + origId);
+        Element next = origBtn.iterator().next();
 
         System.out.println("/html/body/a");
     }
